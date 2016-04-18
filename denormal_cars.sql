8ff3e2ce7c3a9497f9b90b35060b509bbe84de52
maliaoreta <maliaoreta@gmail.com>
2016-04-18 09:45:16 -1000

    adds DISTINCT for DC 7 to get 136 rows and adds DC 8 query

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -1,5 +1,7 @@
-SELECT DISTINCT make_title FROM car_models;
+-- SELECT DISTINCT make_title FROM car_models;
 
-SELECT DISTINCT model_title FROM car_models WHERE make_code = 'VOLKS';
+-- SELECT DISTINCT model_title FROM car_models WHERE make_code = 'VOLKS';
 
-SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+-- SELECT DISTINCT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+
+-- SELECT DISTINCT * FROM car_models WHERE year BETWEEN 2010 AND 2015;
