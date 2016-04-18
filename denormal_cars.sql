757fda80ae0ab9066fc095abd92cc26189937e21
maliaoreta <maliaoreta@gmail.com>
2016-04-18 09:41:32 -1000

    runs query from Denormal Cars 7

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -1,3 +1,5 @@
 SELECT DISTINCT make_title FROM car_models;
 
 SELECT DISTINCT model_title FROM car_models WHERE make_code = 'VOLKS';
+
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
