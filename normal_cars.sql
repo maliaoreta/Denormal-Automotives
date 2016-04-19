5f7f4cadc7e982ed60fc60633fb10051437c5fa7
maliaoreta <maliaoreta@gmail.com>
2016-04-18 19:05:41 -1000

    inserts rows with ids from make_codes_titles, model_codes_titles, and years for 'ACURA'

--- normal_cars.sql
+++ normal_cars.sql
@@ -6,6 +6,27 @@
 -- ALTER TABLE model_codes_titles ADD COLUMN
 --   id SERIAL PRIMARY KEY;
 
-CREATE TABLE years AS SELECT DISTINCT year FROM car_models ORDER BY year ASC;
-ALTER TABLE years ADD COLUMN
-  id SERIAL PRIMARY KEY;
+-- CREATE TABLE years AS SELECT DISTINCT year FROM car_models ORDER BY year ASC;
+-- ALTER TABLE years ADD COLUMN
+--   id SERIAL PRIMARY KEY;
+
+-- CREATE TABLE cars (
+--   id SERIAL PRIMARY KEY,
+--   make_id INTEGER REFERENCES make_codes_titles,
+--   model_id INTEGER REFERENCES model_codes_titles,
+--   year_id INTEGER REFERENCES years
+-- );
+
+INSERT INTO cars (make_id, model_id, year_id)
+  SELECT 
+  (SELECT id FROM make_codes_titles WHERE make_code = 'ACURA'),
+  (SELECT id FROM model_codes_titles WHERE model_code = 'CL_MODELS'),
+  id
+  FROM years
+  WHERE year > 1999;
+
+
+
+
+-- sample subquery:
+-- SELECT (SELECT id FROM make_codes_titles WHERE make_code = 'ACURA') AS make_id
