4fed2b24a159cd4098db043912a93a03b8cfd6b9
maliaoreta <maliaoreta@gmail.com>
2016-04-18 15:07:20 -1000

    creates table years from car_models

--- normal_cars.sql
+++ normal_cars.sql
@@ -2,6 +2,10 @@
 -- ALTER TABLE make_codes_titles ADD COLUMN
 --   id SERIAL PRIMARY KEY;
 
-CREATE TABLE model_codes_titles AS SELECT DISTINCT model_code, model_title FROM car_models;
-ALTER TABLE model_codes_titles ADD COLUMN
+-- CREATE TABLE model_codes_titles AS SELECT DISTINCT model_code, model_title FROM car_models;
+-- ALTER TABLE model_codes_titles ADD COLUMN
+--   id SERIAL PRIMARY KEY;
+
+CREATE TABLE years AS SELECT DISTINCT year FROM car_models;
+ALTER TABLE years ADD COLUMN
   id SERIAL PRIMARY KEY;
