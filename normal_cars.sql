94ef4b68a7c7aa6f6bf66040eb75724bd41028e5
maliaoreta <maliaoreta@gmail.com>
2016-04-18 15:05:32 -1000

    creates model_codes_titles table from car_models

--- normal_cars.sql
+++ normal_cars.sql
@@ -1,4 +1,7 @@
-CREATE TABLE make_codes_titles AS SELECT DISTINCT make_code, make_title FROM car_models;
+-- CREATE TABLE make_codes_titles AS SELECT DISTINCT make_code, make_title FROM car_models;
+-- ALTER TABLE make_codes_titles ADD COLUMN
+--   id SERIAL PRIMARY KEY;
 
-ALTER TABLE make_codes_titles ADD COLUMN
+CREATE TABLE model_codes_titles AS SELECT DISTINCT model_code, model_title FROM car_models;
+ALTER TABLE model_codes_titles ADD COLUMN
   id SERIAL PRIMARY KEY;
